4470225bb0fcc80d21cd1cedc5a881990e42ed81
Charmis <charmis.mariwala@gmail.com>
2026-02-14 16:49:45 +0530

    Improved Streamlit classification report display

--- app.py
+++ app.py
@@ -197,9 +197,13 @@ if uploaded_file is not None:
 
 # Show Classification Report
     st.subheader("Classification Report")
+    report = classification_report(y_test, y_pred, output_dict=True)
+    report_df = pd.DataFrame(report).transpose()
+    st.dataframe(report_df.style.format("{:.2f}"))
+   
 
-    report = classification_report(y, y_pred)
-    st.text(report)
+    
+    #st.text(report)
 
 # Combine X_test and y_test
 test_data = pd.DataFrame(X_test, columns=X.columns)
